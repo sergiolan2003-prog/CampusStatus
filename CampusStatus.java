b2e81d1796ff9453a165c8ea0c06382390c5855c
Sergio Lancheros <sergiolan2003@gmail.com>
2026-09-08 14:07:30 -0500

    fix: Se incluye el nombre de la intancia en el mensaje de estado

--- CampusStatus.java
+++ CampusStatus.java
@@ -1,5 +1,5 @@
 public class CampusStatus {
     public static void main(String[] args) {
-        System.out.println("Campus Digital disponible");
+        System.out.println("Campus Digital disponible -  Instancia: Backend Sesion 4 Grupo 1");
     }
 }
